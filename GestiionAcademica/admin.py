6e0064eed794ab2aad98c6c8b5974427b489109d
PerlaGCastillo <contactoperlagodinez@gmail.com>
2023-03-30 18:02:12 -0600

    gestor de alumnos y matriculas

---
+++ GestiionAcademica/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from GestiionAcademica.models import *
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
---
+++ GestiionAcademica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestiionacademicaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GestiionAcademica'
